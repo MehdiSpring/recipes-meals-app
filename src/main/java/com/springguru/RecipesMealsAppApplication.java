915e32315d4bea774eb2aae1e3382f7e8c151573
MehdiSpring <mehdi.boumzi@gmail.com>
2020-05-09 12:22:29 +0200

    Project initialized

---
+++ src/main/java/com/springguru/RecipesMealsAppApplication.java
@@ -0,0 +1,13 @@
+package com.springguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesMealsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesMealsAppApplication.class, args);
+	}
+
+}
